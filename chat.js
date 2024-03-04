33359662168c142ccff0b61a9b4a5816dce4ecb0
allyyblack <allyyblack@gmail.com>
2024-03-04 16:19:31 -0700

    chat

--- chat.js
+++ chat.js
@@ -1,5 +1,16 @@
 let chat = 'Carpe diem';
+
+function sendMessage() {
+  const userMessage = document.getElementById('user-message').value;
+  if (userMessage.trim() !== '') {
+    chat += `<br>You: ${userMessage}`;
+    document.querySelector('#chat-response').innerHTML = chat;
+    // You can also clear the input field after sending a message
+    document.getElementById('user-message').value = '';
+  }
+}
+
 setInterval(() => {
-  chat = `${chat} lorum ipsum`;
-  document.querySelector('#chat-response').textContent = chat;
+  chat += '<br>Lorum ipsum';
+  document.querySelector('#chat-response').innerHTML = chat;
 }, 5000);
