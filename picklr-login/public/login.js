b56344af9e814fe314b28ccd108751b645481a1e
allyyblack <allyyblack@gmail.com>
2024-03-22 11:40:38 -0600

    try

--- picklr-login/public/login.js
+++ picklr-login/public/login.js
@@ -1,62 +1,72 @@
-const users = [];
-
-async function login() {
-    try {
-        const response = await fetch('/api/connect', {
-          method: 'POST',
-          headers: {'content-type': 'application/json'},
-          body: JSON.stringify(),
-        });
+(async () => {
+    const userName = localStorage.getItem('userName');
+    if (userName) {
+      document.querySelector('#playerName').textContent = userName;
+      setDisplay('loginControls', 'none');
+      setDisplay('playControls', 'block');
+    } else {
+      setDisplay('loginControls', 'block');
+      setDisplay('playControls', 'none');
+    }
+  })();
   
-        // Store what the service gave us as the high scores
-        const success = await response.json();
-
-        if (success) {
-            const nameEl = document.querySelector("#name");
-            const imageLinkEl = document.querySelector("#imageLink");
-        
-            const user = {
-                name: nameEl.value,
-                imageLink: imageLinkEl.value
-            };
-        
-            users.push(user);
-        
-            localStorage.setItem("users", JSON.stringify(users));
-        
-            window.location.href = "connect.html";
-        }
-      } catch {
-        console.log("This shouldn't happen!! teehee💩❤️")
-      }
-
-    success = response.json();
-}
-
-document.addEventListener("DOMContentLoaded", function () {
-    const storedUserName = localStorage.getItem("userName");
-    const storedImageLink = localStorage.getItem("imageLink");
-
-    if (storedUserName && storedImageLink) {
-        const userNameDisplay = document.getElementById("userNameDisplay");
-        const userImage = document.querySelector("ul li img");
-
-        userNameDisplay.textContent = storedUserName;
-        userImage.src = storedImageLink;
+  async function loginUser() {
+    loginOrCreate(`/api/auth/login`);
+  }
+  
+  async function createUser() {
+    loginOrCreate(`/api/auth/create`);
+  }
+  
+  async function loginOrCreate(endpoint) {
+    const userName = document.querySelector('#userName')?.value;
+    const password = document.querySelector('#userPassword')?.value;
+    const response = await fetch(endpoint, {
+      method: 'post',
+      body: JSON.stringify({ email: userName, password: password }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    });
+  
+    if (response.ok) {
+      localStorage.setItem('userName', userName);
+      window.location.href = 'play.html';
+    } else {
+      const body = await response.json();
+      const modalEl = document.querySelector('#msgModal');
+      modalEl.querySelector('.modal-body').textContent = `⚠ Error: ${body.msg}`;
+      const msgModal = new bootstrap.Modal(modalEl, {});
+      msgModal.show();
     }
-
-    const storedUsers = localStorage.getItem("users");
-
-    if (storedUsers) {
-        const parsedUsers = JSON.parse(storedUsers);
-
-        parsedUsers.forEach(function (user) {
-            const userList = document.getElementById("userList");
-
-            const listItem = document.createElement("li");
-            listItem.textContent = user.name;
-
-            userList.appendChild(listItem);
-        });
+  }
+  
+  function play() {
+    window.location.href = 'play.html';
+  }
+  
+  function logout() {
+    localStorage.removeItem('userName');
+    fetch(`/api/auth/logout`, {
+      method: 'delete',
+    }).then(() => (window.location.href = '/'));
+  }
+  
+  async function getUser(email) {
+    let scores = [];
+    // See if we have a user with the given email.
+    const response = await fetch(`/api/user/${email}`);
+    if (response.status === 200) {
+      return response.json();
     }
-});
+  
+    return null;
+  }
+  
+  function setDisplay(controlId, display) {
+    const playControlEl = document.querySelector(`#${controlId}`);
+    if (playControlEl) {
+      playControlEl.style.display = display;
+    }
+  }
+  
