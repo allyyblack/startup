42f41b7920a851b86027807c1a46ad1ab2d96263
allyyblack <allyyblack@gmail.com>
2024-03-22 11:30:43 -0600

    fix

--- picklr-login/database.js
+++ picklr-login/database.js
@@ -5,9 +5,8 @@ const config = require('./dbConfig.json');
 
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 const client = new MongoClient(url);
-const db = client.db('simon');
+const db = client.db('picklr');
 const userCollection = db.collection('user');
-const scoreCollection = db.collection('score');
 
 // This will asynchronously test the connection and exit the process if it fails
 (async function testConnection() {
@@ -38,20 +37,6 @@ async function createUser(email, password) {
   await userCollection.insertOne(user);
 
   return user;
-}
-
-function addScore(score) {
-  scoreCollection.insertOne(score);
-}
-
-function getHighScores() {
-  const query = { score: { $gt: 0, $lt: 900 } };
-  const options = {
-    sort: { score: -1 },
-    limit: 10,
-  };
-  const cursor = scoreCollection.find(query, options);
-  return cursor.toArray();
 }
 
 module.exports = {
