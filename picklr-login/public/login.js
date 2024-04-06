0d103ec6c387f0f36c339536a5c51ef33554b148
allyyblack <allyyblack@gmail.com>
2024-04-06 11:51:20 -0600

    add in login and create logic

--- picklr-login/public/login.js
+++ picklr-login/public/login.js
@@ -1,10 +1,60 @@
 const users = [];
 
-async function login() {
-    const nameEl = document.querySelector("#name");
+// async function login() {
+//     const nameEl = document.querySelector("#name");
+//     localStorage.setItem("userName", nameEl.value);
+
+//     const imageLinkEl = document.querySelector("#imageLink");
+//     localStorage.setItem("imageLink", imageLinkEl.value);
+
+//     // const passwordEl = document.querySelector("#userPassword");
+//     // localStorage.setItem("userPassword", passwordEl.value);
+
+//     window.location.href = "connect.html";
+//     // try {
+//     //     const response = await fetch('/api/connect', {
+//     //       method: 'POST',
+//     //       headers: {'content-type': 'application/json'},
+//     //       body: JSON.stringify(),
+//     //     });
+  
+//     //     // Store what the service gave us as the high scores
+//     //     const success = await response.json();
+
+//     //     if (success) {
+//     //         const nameEl = document.querySelector("#name");
+//     //         const imageLinkEl = document.querySelector("#imageLink");
+        
+//     //         const user = {
+//     //             name: nameEl.value,
+//     //             imageLink: imageLinkEl.value
+//     //         };
+        
+//     //         users.push(user);
+        
+//     //         localStorage.setItem("users", JSON.stringify(users));
+        
+//     //         window.location.href = "connect.html";
+//     //     }
+//     //   } catch {
+//     //     console.log("This shouldn't happen!! teehee💩❤️")
+//     //   }
+
+//     // success = response.json();
+// }
+
+
+async function createUser() {
+    console.log("HERE2")
+    const nameEl = document.querySelector("#newName");
     localStorage.setItem("userName", nameEl.value);
+
     const imageLinkEl = document.querySelector("#imageLink");
     localStorage.setItem("imageLink", imageLinkEl.value);
+
+    // const passwordEl = document.querySelector("#userPassword");
+    // localStorage.setItem("userPassword", passwordEl.value);
+
     window.location.href = "connect.html";
     // try {
     //     const response = await fetch('/api/connect', {
@@ -41,6 +91,7 @@ async function login() {
 document.addEventListener("DOMContentLoaded", function () {
     const storedUserName = localStorage.getItem("userName");
     const storedImageLink = localStorage.getItem("imageLink");
+
 
     if (storedUserName && storedImageLink) {
         const userNameDisplay = document.getElementById("userNameDisplay");
