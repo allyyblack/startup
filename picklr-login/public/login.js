add34344b7f870c6736301a1473e63e7e2b8bacc
allyyblack <allyyblack@gmail.com>
2024-04-06 12:03:27 -0600

    fix

--- picklr-login/public/login.js
+++ picklr-login/public/login.js
@@ -1,7 +1,44 @@
 const users = [];
 
-async function createUser() {
+async function loginUser() {
+    loginOrCreate(`/api/auth/login`);
+  }
+  
+  async function createUser() {
     loginOrCreate(`/api/auth/create`);
+  }
+
+  async function loginOrCreate(endpoint) {
+    // const userName = document.querySelector('#userName')?.value;
+    // const password = document.querySelector('#userPassword')?.value;
+
+    const nameEl = document.querySelector("#newName");
+    localStorage.setItem("userName", nameEl.value);
+
+    const imageLinkEl = document.querySelector("#imageLink");
+    localStorage.setItem("imageLink", imageLinkEl.value);
+
+    // const passwordEl = document.querySelector("#userPassword");
+    // localStorage.setItem("userPassword", passwordEl.value);
+    
+    const response = await fetch(endpoint, {
+      method: 'post',
+      body: JSON.stringify({ email: userName, password: password }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    });
+  
+    if (response.ok) {
+      localStorage.setItem('userName', userName);
+      window.location.href = 'play.html';
+    } else {
+      const body = await response.json();
+      const modalEl = document.querySelector('#msgModal');
+      modalEl.querySelector('.modal-body').textContent = `⚠ Error: ${body.msg}`;
+      const msgModal = new bootstrap.Modal(modalEl, {});
+      msgModal.show();
+    }
   }
 
 
