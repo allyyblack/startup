ae0e3c481026189fddc27693057421ed4021f508
allyyblack <allyyblack@gmail.com>
2024-04-13 17:46:03 -0600

    chat works on frontend and backend

--- picklr-websocket/index.js
+++ picklr-websocket/index.js
@@ -3,13 +3,15 @@ const bcrypt = require('bcrypt');
 const express = require('express');
 const app = express();
 const DB = require('./database.js');
-const { peerProxy } = require('./peerProxy.js');
+const { WebSocketServer } = require('ws');
 
 const authCookieName = 'token';
 
 // The service port. In production the frontend code is statically hosted by the service on the same port.
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
-
+server = app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
 // JSON body parsing using built-in middleware
 app.use(express.json());
 
@@ -21,6 +23,67 @@ app.use(express.static('picklr-websocket/public'));
 
 // Trust headers that are forwarded from the proxy so we can determine IP addresses
 app.set('trust proxy', true);
+
+
+// Create a websocket object
+const wss = new WebSocketServer({ noServer: true });
+
+// Handle the protocol upgrade from HTTP to WebSocket
+server.on('upgrade', (request, socket, head) => {
+  wss.handleUpgrade(request, socket, head, function done(ws) {
+    wss.emit('connection', ws, request);
+  });
+});
+
+// Keep track of all the connections so we can forward messages
+let connections = [];
+let id = 0;
+
+wss.on('connection', (ws) => {
+  const connection = { id: ++id, alive: true, ws: ws };
+  connections.push(connection);
+
+  // Forward messages to everyone except the sender
+  ws.on('message', function message(data) {
+    connections.forEach((c) => {
+      if (c.id !== connection.id) {
+        c.ws.send(data);
+      }
+    });
+  });
+
+  // Remove the closed connection so we don't try to forward anymore
+  ws.on('close', () => {
+    const pos = connections.findIndex((o, i) => o.id === connection.id);
+
+    if (pos >= 0) {
+      connections.splice(pos, 1);
+    }
+  });
+
+  // Respond to pong messages by marking the connection alive
+  ws.on('pong', () => {
+    connection.alive = true;
+  });
+});
+
+// Keep active connections alive
+setInterval(() => {
+  connections.forEach((c) => {
+    // Kill any connection that didn't respond to the ping last time
+    if (!c.alive) {
+      c.ws.terminate();
+    } else {
+      c.alive = false;
+      c.ws.ping();
+    }
+  });
+}, 10000);
+
+
+
+
+
 
 // Router for service endpoints
 const apiRouter = express.Router();
@@ -104,10 +167,4 @@ function setAuthCookie(res, authToken) {
     sameSite: 'strict',
   });
 }
-
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
-
-peerProxy(httpService);
 
