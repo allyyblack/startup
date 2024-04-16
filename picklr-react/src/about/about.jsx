eb693ec82a0b52efb44053b2846f7e13de7b530a
allyyblack <allyyblack@gmail.com>
2024-04-15 20:08:39 -0600

    about works

--- picklr-react/src/about/about.jsx
+++ picklr-react/src/about/about.jsx
@@ -4,8 +4,11 @@ export function About({ userName, authState, onAuthChange }) {
   return (
     <main>
       <div>
-        about displayed here!
-      </div>
+      <img src="https://images.unsplash.com/photo-1687102618656-907b757ad5d9?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fHBpY2tsZWJhbGx8ZW58MHx8MHx8fDA%3D" alt="Pickleball"/>
+      <p>
+        Picklr is an application to connect pickleball players based on their rankings. After playing with a picklr user in person, you will be able to rank them on a scale from 1-5. Enjoy pickling!
+      </p>
+    </div>
     </main>
   );
 }
